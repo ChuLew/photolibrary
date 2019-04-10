8451b26438f9dc13c1de4ceed7945236c5b8950e
Mitch Gin Lew <mitchginlew@gmail.com>
2019-04-10 12:09:12 -0400

    initial commit of full repository

---
+++ Photo65/src/photo65/view/Administrator.java
@@ -0,0 +1,5 @@
+package photo65.view;
+
+public class Administrator {
+
+}
---
+++ Photo65/src/photo65/view/Login.java
@@ -0,0 +1,21 @@
+package photo65.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+/***
+ * Controller class for loginPage.fxml
+ * @author Mitch Lew
+ */
+
+public class Login implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
